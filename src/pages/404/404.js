808f9898592c31ac1790634ae22a1c19a584e11e
Kate Varenik <katyavarenik@mail.ru>
2021-09-09 14:51:11 +0300

    404 page mobile

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,21 +1,36 @@
+import { isMobile } from "react-device-detect";
 import "./404.css";
 import FooterGeneral from "../../components/Footer/FooterGeneral/FooterGeneral";
 import image404 from "../../img/404.svg";
 import HeaderInfo from "../../components/Header/headerInfo/HeaderInfo";
 import HeaderBody from "../../components/Header/headerBody/HeaderBody";
 import HeaderNav from "../../components/Header/headerNav/HeaderNav";
+import HeaderMobile from "../../components/Header/HeaderMobile/HeaderMobile";
+import FooterMobile from "../../components/Footer/FooterMobile/FooterMobile";
 
 export const PageNotFound = () => {
   return (
     <div className="page-not-found">
-      <HeaderInfo />
-      <HeaderBody />
-      <HeaderNav />
-      <div className="page-not-found__content">
-        <img src={image404} alt="Page Not Found" />
-      </div>
+      {isMobile ? (
+        <>
+          <HeaderMobile />
+          <div className="page-not-found__content">
+            <img src={image404} alt="Page Not Found" />
+          </div>
+          <FooterMobile />
+        </>
+      ) : (
+        <>
+          <HeaderInfo />
+          <HeaderBody />
+          <HeaderNav />
+          <div className="page-not-found__content">
+            <img src={image404} alt="Page Not Found" />
+          </div>
 
-      <FooterGeneral />
+          <FooterGeneral />
+        </>
+      )}
     </div>
   );
 };
