72ef61aa1950da34fb6c64a545ffdb523585f93d
Kate Varenik <katyavarenik@mail.ru>
2022-05-23 17:41:31 +0300

    change BrouserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
