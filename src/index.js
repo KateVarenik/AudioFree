4e0f06b7dd48a1e6f31c7cd239f2affc404bd83a
Kate Varenik <katyavarenik@mail.ru>
2022-05-24 00:16:25 +0300

    add sctoll to top

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import "./reset.css";
-import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
+
+import "./reset.css";
+import "./index.css";
+import ScrollToTop from "components/Scroll-to-top";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
